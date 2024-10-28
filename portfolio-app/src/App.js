bb7ce196200bf793e9a9e43dbd7f8229d0772abf
Ameer Nur Saidy <ansnursaidy@gmail.com>
2024-10-28 00:37:38 -0700

    Added Header to HomePage

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
-
+import HomePage from './Homepage';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      Hello World
+      <HomePage/>
       </header>
     </div>
   );
