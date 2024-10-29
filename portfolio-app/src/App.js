8108dea16860e837b9918425820bca86508d10b4
Ameer Nur Saidy <ansnursaidy@gmail.com>
2024-10-28 21:39:10 -0700

    Added Content To Homepage

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Homepage';
+import Header from './Header';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
       <HomePage/>
       </header>
     </div>
