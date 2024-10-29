13a7623aadb967dcf0cf98fe9c0492c27592b126
Ameer Nur Saidy <ansnursaidy@gmail.com>
2024-10-29 12:04:42 -0700

    Added Intro in Homepage

--- portfolio-app/src/Homepage.js
+++ portfolio-app/src/Homepage.js
@@ -13,12 +13,9 @@ function HomePage() {
           <h3 className='text-6xl text-orange-500'>I am Ameer Nur Saidy</h3>
 
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing.
+            A 3rd year Simon Fraser University Computer Science student and an aspiring front-end developer eager
+            to improve my skills. I am excited to showcase my portfolio and demonstrate the projects I have taken a part in. 
+            Navigate to About page to learn a little more about me or the Project page to see some of the projects I have made.
           </p>
         </div>
       </div>
