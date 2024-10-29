0d982bf29c3104b2b40e7f6eb4de33c887220d66
Ameer Nur Saidy <ansnursaidy@gmail.com>
2024-10-29 02:15:43 -0700

    Multiple Project Cards Working

--- portfolio-app/src/Projects.js
+++ portfolio-app/src/Projects.js
@@ -3,10 +3,28 @@ import fractalImage from './images/fractal.jpg'
 function Projects() {
   return (
     <div className="flex justify-center py-10">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg ">
         <img src={fractalImage} alt="Fractal" className="w-full h-auto rounded" />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">Fractals Project</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-10">
+        <img src={fractalImage} alt="Fractal" className="w-full h-auto rounded" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Report Project</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg ">
+        <img src={fractalImage} alt="Fractal" className="w-full h-auto rounded" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Priority Queue</div>
           <p class="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
           </p>
