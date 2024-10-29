23322ab958a32da05222fa46258686af99260856
Ameer Nur Saidy <ansnursaidy@gmail.com>
2024-10-28 22:23:59 -0700

    Arranged Content Side by Side

--- portfolio-app/src/Homepage.js
+++ portfolio-app/src/Homepage.js
@@ -5,11 +5,14 @@ function HomePage() {
   return (
     <div>
      <Header/>
-     <div>
+     <div className="flex items-center">
+        <div className="w-1/3 p-5">
      <img src= {profileImage} alt="Profile" className="h-auto rounded" />
-   
+     </div>
+     <div className="w-2/3 p-5">
         <h2>HELLO</h2>
         <h3 className='text-6xl text-orange-500'>I am Ameer Nur Saidy</h3>
+        
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
@@ -18,9 +21,9 @@ function HomePage() {
           electronic typesetting, remaining essentially unchanged. It was popularised in 
           the 1960s with the release of Letraset sheets containing.
         </p>
+         </div>
         </div>
-        </div>
-    
+    </div>
   );
 }
 
