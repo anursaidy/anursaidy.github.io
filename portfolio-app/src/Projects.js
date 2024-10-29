4a16e3a8925ed09e4e5d3319119a014fa2e56ddc
Ameer Nur Saidy <ansnursaidy@gmail.com>
2024-10-29 03:32:42 -0700

    Added Images & Links to Cards

--- portfolio-app/src/Projects.js
+++ portfolio-app/src/Projects.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import fractalImage from './images/fractal.jpg'
+import reportImage from './images/report.png'
+import matrixLL from './images/matrixLL.jpg'
 function Projects() {
   return (
     <div className="flex justify-center py-10">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src={fractalImage} alt="Fractal" className="w-full h-auto rounded" />
-        <div class="px-6 py-4">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg w-1/2">
+        <img src={fractalImage} alt="Fractal" className="w-full h-1/2 rounded" />
+        <div class="px-6 py-4 flex-grow">
           <div class="font-bold text-xl mb-2">Fractals Project</div>
           <p class="text-gray-700 text-base">
-            Displays Fractals from a Julia and Mandelbrot PPM.
+            Displays Fractals  of Julia and Mandelbrot PPM.
           </p>
+          <a href="https://github.com/anursaidy/Fractals-Lab"
+            className="text-blue-500 inline-block mt-5">
+            View on GitHub
+          </a>
         </div>
       </div>
-      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-10">
-        <img src={fractalImage} alt="Fractal" className="w-full h-auto rounded" />
-        <div class="px-6 py-4">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-10 w-1/2">
+        <img src={reportImage} alt="Report" className="w-full h-1/2 rounded" />
+        <div class="px-6 py-4 flex-grow">
           <div class="font-bold text-xl mb-2">Report Project</div>
           <p class="text-gray-700 text-base">
             Create and display reports.
           </p>
+          <a href="https://github.com/anursaidy/report-application"
+            className="text-blue-500 inline-block mt-5">
+            View on GitHub
+          </a>
         </div>
       </div>
-      <div class="max-w-sm rounded overflow-hidden shadow-lg ">
-        <img src={fractalImage} alt="Fractal" className="w-full h-auto rounded" />
+      <div class="max-w-sm rounded overflow-hidden shadow-lg w-1/2">
+        <img src={matrixLL} alt="Matrix Linked List" className="w-full h-1/2 rounded" />
         <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Priority Queue</div>
+          <div class="font-bold text-xl mb-2">Matrix Linked List</div>
           <p class="text-gray-700 text-base">
-            A fully functioning priority queue.
+            A fully functioning Linked List using a Matrix.
           </p>
+          <a href="https://github.com/anursaidy/Matrix-Linked-List"
+            className="text-blue-500 inline-block mt-5">
+            View on GitHub
+          </a>
         </div>
       </div>
     </div>
